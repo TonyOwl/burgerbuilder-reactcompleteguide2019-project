efb0813a9b46413c93ed45d3fe4a83a7d341a2b8
Tony Owl <antoniovaldenisio@gmail.com>
2019-11-28 10:37:46 -0300

    18 - Burger Builder: Adding Authentication

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -5,3 +5,5 @@ export {
 } from "./burgerBuilder";
 
 export { purchaseBurger, purchaseInit, fecthOrders } from "./order";
+
+export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth";
