d75a841dd53d32fe1b9960b324ce94bc67da7e13
Tony Owl <antoniovaldenisio@gmail.com>
2019-12-03 15:51:23 -0300

    25 - Burger Builder: Redux-Saga

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,5 +1,4 @@
 import * as actionTypes from "./actionTypes";
-import axios from "../../axios-orders";
 
 export const addIngredient = name => {
   return {
@@ -29,14 +28,7 @@ export const fecthIngredientsFail = () => {
 };
 
 export const initIngredients = () => {
-  return dispatch => {
-    axios
-      .get("/ingredients.json")
-      .then(response => {
-        dispatch(setIngredients(response.data));
-      })
-      .catch(err => {
-        dispatch(fecthIngredientsFail());
-      });
+  return {
+    type: actionTypes.INIT_INGRIDIENTS
   };
 };
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,9 +1,31 @@
 export {
   addIngredient,
   removeIngredient,
-  initIngredients
+  initIngredients,
+  setIngredients,
+  fecthIngredientsFail
 } from "./burgerBuilder";
 
-export { purchaseBurger, purchaseInit, fecthOrders } from "./order";
+export {
+  purchaseBurger,
+  purchaseInit,
+  purchaseBurgerStart,
+  purchaseBurgerFail,
+  purchaseBurgerSuccess,
+  fecthOrders,
+  fetchOrdersStart,
+  fetchOrdersFail,
+  fetchOrdersSuccess
+} from "./order";
 
-export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth";
+export {
+  auth,
+  logout,
+  setAuthRedirectPath,
+  authCheckState,
+  logoutSucceed,
+  authStart,
+  authSuccess,
+  checkAuthTimeout,
+  authFail
+} from "./auth";
